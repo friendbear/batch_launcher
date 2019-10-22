3383adbaaddf078a42a555695508978767240569
friendbear <friendbear@users.noreply.github.com>
2019-10-22 17:17:23 +0900

    Add Jar

---
+++ lib/batch/Main.java
@@ -0,0 +1,12 @@
+package batch;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Arrays.asList(args).forEach(v -> System.out.println(v));
+
+        System.exit(0);
+    }
+}
